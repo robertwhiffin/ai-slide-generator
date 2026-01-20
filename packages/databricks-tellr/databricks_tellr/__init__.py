7ab328c4647aa3d90386a0438cd5aa9294bbb15d
robert.whiffin <robert.whiffin@databricks.com>
2026-01-20 17:21:23 +0000

    doc update

--- packages/databricks-tellr/databricks_tellr/__init__.py
+++ packages/databricks-tellr/databricks_tellr/__init__.py
@@ -1,5 +1,6 @@
 """Tellr deployment package for Databricks Apps."""
 
+from databricks_tellr import deploy
 from databricks_tellr.deploy import create, delete, update
 
-__all__ = ["create", "update", "delete"]
+__all__ = ["deploy", "create", "update", "delete"]
